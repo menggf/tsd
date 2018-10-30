f3641ddb64f886615bbf0e51fbc4d1f1aaf9192c
menggf <menggf@gmail.com>
2018-10-30 18:07:13 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -2,5 +2,5 @@
 # TSD need user to install BWA and build the reference index firstly. 
 # Please replace the -G with your own one !!!
 
-perl LongAssembly.pl -d . -s sampleData/input.fq -G /mnt/work/genome/hg38.fa -i sampleData/virus.fa
+perl LongAssembly.pl -d . -s sampleData/input.fastq -G /mnt/work/genome/hg38.fa -i sampleData/hbv.fa
 
