d17f7255e1caff182a769420403b3f68a25a8b27
Guofeng Meng <menggf@gmail.com>
2019-02-19 10:28:38 +0800

    20190219

--- example.sh
+++ example.sh
@@ -1,6 +1,10 @@
 
 # TSD need user to install BWA and build the reference index firstly. 
+
+# This example data will take 1-2 min
+
 # Please replace the -G with your own one !!!
+
 
 perl LongAssembly.pl -d . -s sampleData/input.fastq -G /mnt/work/genome/hg38.fa -i sampleData/hbv.fa
 
